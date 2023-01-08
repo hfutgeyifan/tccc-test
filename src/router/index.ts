1b1b48776e73f517a796d50e581b214c1f7ba750
shyfge <shyfge@tencent.com>
2023-01-08 22:12:43 +0800

    1.0.3 success

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
 
 const routes: Array<RouteRecordRaw> = [
@@ -8,17 +8,14 @@ const routes: Array<RouteRecordRaw> = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/chat',
+    name: 'Chat',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Chat.vue')
   }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 })
 
---
+++ src/views/Chat.vue
@@ -0,0 +1,16 @@
+<template>
+  <TCCCWebChat></TCCCWebChat>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { TCCCWebChat } from "tccc-vue-uikit";
+export default defineComponent({
+  components: {
+    TCCCWebChat,
+  },
+  data() {
+    return {};
+  },
+});
+</script>
