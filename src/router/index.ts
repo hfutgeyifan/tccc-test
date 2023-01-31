27628351a66330ee3719a015a2a4ebe2ad241c1b
shyfge <shyfge@tencent.com>
2023-01-31 17:01:55 +0800

    test

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,10 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHashHistory, RouteRecordRaw, createWebHistory } from 'vue-router'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: import(/* webpackChunkName: "about" */ '../views/Home.vue')
   },
   {
     path: '/chat',
